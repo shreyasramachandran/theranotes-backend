e768ca654aafa6a95ac82236d353038bb16a6db8
shreyasramachandran <shreyasramachandran@gmail.com>
2024-01-25 05:15:56 +0000

    making connection secure

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('./secrets/server.key'),
-    cert: fs.readFileSync('./secrets/server.cert'),
+    key: fs.readFileSync('/etc/letsencrypt/live/api.islandstones.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.islandstones.xyz/fullchain.pem'),
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
