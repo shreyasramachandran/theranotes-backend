897bc549c4c8d5f9f9aa5596023cd845155c3872
Subhash Kashyap <sk.kashyap1997@gmail.com>
2024-01-26 13:29:52 +0530

    local changes

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/api.islandstones.xyz/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api.islandstones.xyz/fullchain.pem'),
+    key: fs.readFileSync('./secrets/server.key'),
+    cert: fs.readFileSync('./secrets/server.cert'),
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
   });
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
